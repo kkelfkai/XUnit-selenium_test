e03d6d6df75d97a683aaa782bc5b5bc32e7f832a
kkelfkai <17778190083@163.com>
2019-10-31 14:18:23 +0800

    add @Before @After @BeforeClass @AfterClass

--- src/test/java/TestJUnit4Demo1.java
+++ src/test/java/TestJUnit4Demo1.java
@@ -1,9 +1,31 @@
-import org.junit.Test;
+import org.junit.*;
+import org.junit.runners.MethodSorters;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)   //显示指定运行case的顺序按照AscenII排序。虽然默认也会按照反射的字节码排序。但是一般集成测试用例的需要按照一定的顺序执行的。
 public class TestJUnit4Demo1 {
+
+	@BeforeClass
+	public static void beforeClass(){
+		System.out.println("beforeAllTestcase, @BeforeClass");
+	}
+
+	@AfterClass
+	public static void afterClass(){
+		System.out.println("afterAllTestcase, @AfterClass");
+	}
+
+	@Before
+	public void before(){
+		System.out.println("i am @before");
+	}
+
+	@After
+	public void after(){
+		System.out.println("i am @after");
+	}
 
 	@Test
 	public void demo1(){

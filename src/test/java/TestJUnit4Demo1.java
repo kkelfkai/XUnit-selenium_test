08fae77a79e4aca38320d534a9bc5e76d98966e7
kkelfkai <17778190083@163.com>
2019-10-31 13:52:27 +0800

    first commit

--- src/test/java/TestJUnit4Demo1.java
+++ src/test/java/TestJUnit4Demo1.java
@@ -1,8 +1,26 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class TestJUnit4Demo1 {
 
 	@Test
 	public void demo1(){
+		System.out.println("demo1");
+		assertTrue(true);
+	}
 
+	@Test
+	public void demo2(){
+		System.out.println("demo2");
+		assertTrue(false);
+	}
+
+	@Test
+	public void demo3(){
+		System.out.println("demo3");
+		assertEquals(1,1);
 	}
 
 }
